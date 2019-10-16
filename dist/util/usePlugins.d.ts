bab60cb2f258978d5914b3c7340c31e9efbb03ef
jaames <jamesdaniel2197@gmail.com>
2019-10-16 19:08:09 +0100

    remove undeeded iro.js files

--- dist/util/usePlugins.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * iro.js plugins API
- * This provides the iro.use method, which can be used to register plugins which extend the iro.js core
- */
-export declare function usePlugins(core: any): any;
