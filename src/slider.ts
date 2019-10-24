7f35c1ed000e42e9f4bd06d158af2da4a8f053cb
jaames <jamesdaniel2197@gmail.com>
2019-10-24 14:08:33 +0100

    port vertical sliders, svg url resolution fix

--- src/slider.ts
+++ src/slider.ts
@@ -1,20 +1,78 @@
 import { IroColor } from './color';
+import { IroColorPickerOptions } from './colorPickerOptions';
 
-export function getSliderDimensions(props: any) {
-  let { width, sliderHeight, borderWidth, handleRadius, padding } = props;
-  sliderHeight = sliderHeight ? sliderHeight : padding * 2 + handleRadius * 2 + borderWidth * 2;
+export type SliderShape = 'bar' | 'circle' | '';
+export type SliderType = 'hue' | 'saturation' | 'value' | 'temperature' | '';
+
+export interface SliderOptions extends IroColorPickerOptions {
+  color: IroColor;
+  sliderShape: SliderShape;
+  sliderType: SliderType;
+  minTemperature: number;
+  maxTemperature: number;
+}
+
+export const sliderDefaultOptions = {
+  sliderShape: 'bar',
+  sliderType: 'value',
+  minTemperature: 2200,
+  maxTemperature: 11000
+}
+
+/**
+ * @desc Get the CSS styles for the slider root
+ * @param props - slider props
+ */
+export function getSliderStyles(props: Partial<SliderOptions>) {
   return {
-    radius: sliderHeight / 2,
-    x: 0,
-    y: 0,
-    width: width,
-    height: sliderHeight,
+    [props.layoutDirection === 'vertical' ? 'marginLeft' : 'marginTop']: props.sliderMargin
   }
 }
 
-export function getCurrentSliderValue(props: any) {
+/**
+ * @desc Get the bounding dimensions of the slider
+ * @param props - slider props
+ */
+export function getSliderDimensions(props: Partial<SliderOptions>) {
+  let { width, sliderHeight, borderWidth, handleRadius, padding, sliderShape } = props;
+  const isVertical = props.layoutDirection === 'vertical';
+  // automatically calculate sliderHeight if its not defined
+  sliderHeight = sliderHeight ? sliderHeight : padding * 2 + handleRadius * 2 + borderWidth * 2;
+  const handleRange = width - sliderHeight;
+  if (sliderShape === 'circle') {
+    return {
+      handleRange,
+      width: width,
+      height: width,
+      cx: width / 2,
+      cy: width / 2,
+      radius: width / 2 - borderWidth / 2
+    }
+  } else {
+    return {
+      handleRange,
+      radius: sliderHeight / 2,
+      x: 0,
+      y: 0,
+      width: isVertical ? sliderHeight : width,
+      height: isVertical ? width : sliderHeight,
+    }
+  }
+}
+
+/**
+ * @desc Get the current slider value as a percentage
+ * @param props - slider props
+ */
+export function getCurrentSliderValue(props: Partial<SliderOptions>) {
   const hsv = props.color.hsv;
   switch (props.sliderType) {
+    case 'temperature':
+      const { minTemperature, maxTemperature } = props;
+      const temperatureRange = maxTemperature - minTemperature;
+      const percent = ((props.color.kelvin - minTemperature) / temperatureRange) * 100;
+      // clmap percentage
+      return Math.max(0, Math.min(percent, 100));
     case 'hue':
       return hsv.h /= 3.6;
     case 'saturation':
@@ -25,13 +83,29 @@ export function getCurrentSliderValue(props: any) {
   }
 }
 
-export function getSliderValueFromInput(props: any, x: number, y: number, bounds) {
-  const handleRange = bounds.width - bounds.height;
-  const cornerRadius = bounds.height / 2;
-  let dist = x - (bounds.left + cornerRadius);
-  dist = Math.max(Math.min(dist, handleRange), 0);
-  const percent = Math.round((100 / handleRange) * dist);
+/**
+ * @desc Get the current slider value from user input
+ * @param props - slider props
+ * @param x - global input x position
+ * @param y - global input y position
+ * @param bounds - slider element bounding box
+ */
+export function getSliderValueFromInput(props: Partial<SliderOptions>, x: number, y: number, bounds: any) {
+  const { handleRange, radius } = getSliderDimensions(props);
+  let handlePos;
+  if (props.layoutDirection === 'vertical') {
+    handlePos = -1 * (y - bounds.top) + handleRange + radius;
+  } else {
+    handlePos = x - (bounds.left + radius);
+  }
+  // clamo handle position
+  handlePos = Math.max(Math.min(handlePos, handleRange), 0);
+  const percent = Math.round((100 / handleRange) * handlePos);
   switch (props.sliderType) {
+    case 'temperature':
+      const { minTemperature, maxTemperature } = props;
+      const temperatureRange = maxTemperature - minTemperature;
+      return minTemperature + temperatureRange * (percent / 100);
     case 'hue':
       return percent * 3.6;
     default:
@@ -39,24 +113,35 @@ export function getSliderValueFromInput(props: any, x: number, y: number, bounds
   }
 }
 
-export function getSliderHandlePosition(props: any) {
-  const { width, height, radius } = getSliderDimensions(props);
+/**
+ * @desc Get the current slider position
+ * @param props - slider props
+ */
+export function getSliderHandlePosition(props: Partial<SliderOptions>) {
+  const { width, height, radius, handleRange } = getSliderDimensions(props);
+  const isVertical = props.layoutDirection === 'vertical';
   const sliderValue = getCurrentSliderValue(props);
-  const handleRange = width - radius * 2;
-  const x = radius + (sliderValue / 100) * handleRange;
-  const y = height / 2;
-  return {x, y};
+  const midPoint = isVertical ? width / 2 : height / 2;
+  let handlePos = radius + (sliderValue / 100) * handleRange;
+  if (isVertical) {
+    handlePos = -1 * handlePos + handleRange + radius * 2;
+  } 
+  return {x: isVertical ? midPoint : handlePos, y: isVertical ? handlePos : midPoint};
 }
 
-export function getSliderGradient(props: any) {
+/**
+ * @desc Get the gradient stops for a slider
+ * @param props - slider props
+ */
+export function getSliderGradient(props: Partial<SliderOptions>) {
   const hsv = props.color.hsv;
 
   switch (props.sliderType) {
-    case 'kelvin':
+    case 'temperature':
       const stops = [];
-      const min = 3000;
-      const max = 9000;
-      const numStops = 12;
+      const min = props.minTemperature;
+      const max = props.maxTemperature;
+      const numStops = 8;
       const range = max - min;
       for (let kelvin = min, stop = 0; kelvin < max; kelvin += range / numStops, stop += 1) {
         const { r, g, b } = IroColor.kelvinToRgb(kelvin);
@@ -87,5 +172,19 @@ export function getSliderGradient(props: any) {
         [0, '#000'],
         [100, `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`]
       ];
+  }
+}
+
+/**
+ * @desc Get the gradient coords for a slider
+ * @param props - slider props
+ */
+export function getSliderGradientCoords(props: Partial<SliderOptions>) {
+  const isVertical = props.layoutDirection === 'vertical';
+  return {
+    x1: '0%',
+    y1: isVertical ? '100%' : '0%',
+    x2: isVertical ? '0%' : '100%',
+    y2: '0%'
   }
 }
--- src/svg.ts
+++ src/svg.ts
@@ -1,24 +1,36 @@
+// Keep track of html <base> elements for resolveSvgUrl
+// getElementsByTagName returns a live HTMLCollection, which stays in sync with the DOM tree
+// So it only needs to be called once
+const BASE_ELEMENTS = document.getElementsByTagName('base');
+
 /**
- * Resolve an SVG URL
- * This is required to work around how Safari handles gradient URLS under certain conditions
+ * @desc Resolve an SVG reference URL
+ * This is required to work around how Safari and iOS webviews handle gradient URLS under certain conditions
  * If a page is using a client-side routing library which makes use of the HTML <base> tag, 
  * Safari won't be able to render SVG gradients properly (as they are referenced by URLs)
  * More info on the problem: 
  * https://stackoverflow.com/questions/19742805/angular-and-svg-filters/19753427#19753427
  * https://github.com/jaames/iro.js/issues/18
  * https://github.com/jaames/iro.js/issues/45
+ * https://github.com/jaames/iro.js/pull/89
+ * @props url - SVG reference URL
  */
-export function resolveSvgUrl(url) {
+export function resolveSvgUrl(url: string) {
   // Sniff useragent string to check if the user is running Safari
   const ua = window.navigator.userAgent;
   const isSafari = /^((?!chrome|android).)*safari/i.test(ua);
   const isIos = /iPhone|iPod|iPad/i.test(ua);
   const location = window.location;
-  return (isSafari || isIos) ? `${location.protocol}//${location.host}${location.pathname}${location.search}${url}` : url;
+  return ((isSafari || isIos) && (BASE_ELEMENTS.length > 0)) ? `${location.protocol}//${location.host}${location.pathname}${location.search}${url}` : url;
 }
 
 /**
- * Get the path commands to draw an svg arc
+ * @desc Get the path commands to draw an svg arc
+ * @props cx - arc center point x
+ * @props cy - arc center point y
+ * @props radius - arc radius
+ * @props startAngle - arc start angle
+ * @props endAngle - arc end angle
  */
 export function getSvgArcPath(cx: number, cy: number, radius: number, startAngle: number, endAngle: number) {
   const largeArcFlag = endAngle - startAngle <= 180 ? 0 : 1;
